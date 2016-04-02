20ababa381da2683c49db2e353f9bd24d1bf2972
Jesper Plantener <jesper@plantener.dk>
2016-04-02 23:08:59 +0200

    Trying to implement OnChanges.. Not Working

--- src/app/roman-generator/index.ts
+++ src/app/roman-generator/index.ts
@@ -1,1 +1,2 @@
-export * from './roman-generator.component';
+export * from './roman-generator.component'; // What does this do?
+export * from './roman-generator.service'; // What does this do?
--- src/app/roman-generator/roman-generator.component.ts
+++ src/app/roman-generator/roman-generator.component.ts
@@ -1,10 +1,24 @@
-import {Component, Input} from 'angular2/core';
+import {Component, Input, OnChanges, SimpleChange} from 'angular2/core';
+
+import {RomanGeneratorService} from './roman-generator.service';
 
 @Component({
   selector: 'roman-generator',
-  template: require('./roman-generator.component.html')
+  template: require('./roman-generator.component.html'),
+  providers: [RomanGeneratorService]
 })
 
-export class RomanGeneratorComponent{
-  id: number;
+export class RomanGeneratorComponent implements OnChanges{ // OnChanges not working :'(
+  ngOnChanges(changes: {[propName: string]: SimpleChange}) {
+     console.log('ngOnChanges - decimalNumber = ' + changes['decimalNumber'].currentValue);
+   }
+
+  decimalNumber: number;
+  romanNumber: string;
+
+  constructor(private _romanGeneratorService: RomanGeneratorService) { }
+
+  romanize(decimalNmb){
+    this.romanNumber = this._romanGeneratorService.romanize(decimalNmb);
+  }
 }
