8ac0145620b52c1c666532a074b0b79406615bed
Jesper Plantener <jesper@plantener.dk>
2016-04-02 19:48:25 +0200

    Added roman generator component

---
+++ src/app/roman-generator/index.ts
@@ -0,0 +1,1 @@
+export * from './roman-generator.component';
---
+++ src/app/roman-generator/roman-generator.component.ts
@@ -0,0 +1,10 @@
+import {Component, Input} from 'angular2/core';
+
+@Component({
+  selector: 'roman-generator',
+  template: require('./roman-generator.component.html')
+})
+
+export class RomanGeneratorComponent{
+  id: number;
+}
