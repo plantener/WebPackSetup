cf64103286fe03d4f0274afa5731d47ee9cce802
Jesper Plantener <jesper@plantener.dk>
2016-04-03 12:50:41 +0200

    Roman generator working

--- src/app/roman-generator/roman-generator.component.ts
+++ src/app/roman-generator/roman-generator.component.ts
@@ -1,4 +1,5 @@
-import {Component, Input, OnChanges, SimpleChange} from 'angular2/core';
+import {Component, Input, OnInit} from 'angular2/core';
+import {Control} from 'angular2/common';
 
 import {RomanGeneratorService} from './roman-generator.service';
 
@@ -8,17 +9,18 @@ import {RomanGeneratorService} from './roman-generator.service';
   providers: [RomanGeneratorService]
 })
 
-export class RomanGeneratorComponent implements OnChanges{ // OnChanges not working :'(
-  ngOnChanges(changes: {[propName: string]: SimpleChange}) {
-     console.log('ngOnChanges - decimalNumber = ' + changes['decimalNumber'].currentValue);
-   }
-
-  decimalNumber: number;
+export class RomanGeneratorComponent implements OnInit{ //implements OnChanges Neat way. Can i get it to work?
+ngOnInit(){
+  this.decimalNumberControl.valueChanges.subscribe(decimalNumber => this.romanize(decimalNumber));
+  
+}
+  decimalNumberControl = new Control();
   romanNumber: string;
 
-  constructor(private _romanGeneratorService: RomanGeneratorService) { }
+  constructor(private _romanGeneratorService: RomanGeneratorService) {
+  }
 
-  romanize(decimalNmb){
+  romanize(decimalNmb: number){
     this.romanNumber = this._romanGeneratorService.romanize(decimalNmb);
   }
 }
--- src/app/roman-generator/roman-generator.service.ts
+++ src/app/roman-generator/roman-generator.service.ts
@@ -2,7 +2,15 @@ import {Injectable} from 'angular2/core';
 
 @Injectable()
 export class RomanGeneratorService{
+
   romanize(decimalNumber: number){
-    return 'MCD';
+    var lookup = {M:1000,CM:900,D:500,CD:400,C:100,XC:90,L:50,XL:40,X:10,IX:9,V:5,IV:4,I:1},roman = '',i;
+    for ( i in lookup ) {
+      while ( decimalNumber >= lookup[i] ) {
+        roman += i;
+        decimalNumber -= lookup[i];
+      }
+    }
+    return roman;
   }
 }
